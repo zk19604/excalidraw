47ff41a50c44b07bd062e5a2f5cc53f6172d303a
Mark Tolmacs <mark@lazycat.hu>
2026-03-26 16:26:28 +0000

    fix: Fixes
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -8839,13 +8839,16 @@ class App extends React.Component<AppProps, AppState> {
           ) as any;
 
           if (arrow && isBindingElement(arrow)) {
-            const { hitFocusPoint, pointerOffset } =
-              handleFocusPointPointerDown(
-                arrow,
-                pointerDownState,
-                elementsMap,
-                this.state,
-              );
+            const {
+              hitFocusPoint,
+              pointerOffset,
+              arrowOtherEndpointInitialBinding,
+            } = handleFocusPointPointerDown(
+              arrow,
+              pointerDownState,
+              elementsMap,
+              this.state,
+            );
 
             // If focus point is hit, update state and prevent element selection
             if (hitFocusPoint) {
@@ -8855,6 +8858,10 @@ class App extends React.Component<AppProps, AppState> {
                   hoveredFocusPointBinding: hitFocusPoint,
                   draggedFocusPointBinding: hitFocusPoint,
                   pointerOffset,
+                  initialState: {
+                    ...linearElementEditor.initialState,
+                    arrowOtherEndpointInitialBinding,
+                  },
                 },
               });
               return false;
@@ -11020,14 +11027,19 @@ class App extends React.Component<AppProps, AppState> {
         }
 
         if (this.state.selectedLinearElement.draggedFocusPointBinding) {
-          handleFocusPointPointerUp(
-            this.state.selectedLinearElement,
-            this.scene,
-          );
+          const { arrowOtherEndpointInitialBinding } =
+            handleFocusPointPointerUp(
+              this.state.selectedLinearElement,
+              this.scene,
+            );
           this.setState({
             selectedLinearElement: {
               ...this.state.selectedLinearElement,
               draggedFocusPointBinding: null,
+              initialState: {
+                ...this.state.selectedLinearElement.initialState,
+                arrowOtherEndpointInitialBinding,
+              },
             },
           });
         } else if (
